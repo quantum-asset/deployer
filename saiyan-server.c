07bc7169e82a1203cb2b143c892ac951a3bdf7b5
Ubuntu <ubuntu@ip-172-31-23-36.ec2.internal>
2021-10-29 16:48:41 +0000

    [Primera version daemon que funciona]

--- saiyan-server.c
+++ saiyan-server.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 printf("Iniciando servidor\n");
 fflush(stdout);	
 //if (
-//	becomeDaemon(0); 
+	becomeDaemon(0); 
 printf("ERROR EN DAEMONIZION\n");
 	fflush(stdout);
 //!= 0)	return -1;
